11ecbc704c3374f89fd64f3669c9c9d2e4c1d705
Muhammad Hassan <hassanshakil12@gmail.com>
2023-10-08 20:28:55 +0500

    Navbar Animmation

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ btn.addEventListener('click', function(){
     if(menu.style.opacity == 0){
         menu.style.opacity = 1
         bg.style.filter = 'blur(7px)'
+        menu.style.transform = 'translateX(0%)'
     }
     else{
         menu.style.opacity = 0
         bg.style.filter = 'none'
+        menu.style.transform = 'translateX(100%)'
     }
 })
